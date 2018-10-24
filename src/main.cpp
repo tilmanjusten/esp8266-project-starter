2b88517d111954bea40583beeefda08725920b4a
Tilman Justen <git@tilmanjusten.de>
2018-10-24 15:09:43 +0200

    use outsourced lib for OTA integration

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,12 @@
 #include <ESP8266mDNS.h>
 #include <FS.h>
 #include <WiFiUdp.h>
-#include <ArduinoOTA.h>
+#include <ESP8266OTASetup.h>
 
 #include "secrets.h"
 
 ESP8266WebServer server(HTTP_PORT);
+ESP8266OTASetup ota;
 
 void handleWebRequests404()
 {
@@ -128,47 +129,6 @@ bool loadFromSpiffs(String path)
     return true;
 }
 
-void setupOTA()
-{
-    // Port defaults to 8266
-    ArduinoOTA.setPort(OTA_PORT);
-
-    // Hostname defaults to esp8266-[ChipID]
-    ArduinoOTA.setHostname(OTA_HOSTNAME);
-
-    // No authentication by default
-    ArduinoOTA.setPassword((const char *)OTA_PASSWORD);
-
-    ArduinoOTA.onStart([]() {
-        Serial.println("Start");
-    });
-    ArduinoOTA.onEnd([]() {
-        Serial.println("\nEnd");
-    });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    });
-    ArduinoOTA.onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR)
-            Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR)
-            Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR)
-            Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR)
-            Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR)
-            Serial.println("End Failed");
-    });
-
-    ArduinoOTA.begin();
-
-    Serial.println("OTA Ready.");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-}
-
 void setup()
 {
     Serial.begin(115200);
@@ -178,7 +138,7 @@ void setup()
     wifiSetup();
 
     // OTA
-    setupOTA();
+    ota.setup();
 
     // Webserver
     SPIFFS.begin();
@@ -188,7 +148,7 @@ void setup()
 void loop()
 {
     // OTA
-    ArduinoOTA.handle();
+    ota.handle();
 
     // Webserver
     server.handleClient();
